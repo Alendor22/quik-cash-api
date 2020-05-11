970867e4e7002e9b07da294d77f0fcdabe5d6e37
Adeja <alendor6@gmail.com>
2020-05-11 01:45:00 -0400

    update relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,11 @@
 class User < ApplicationRecord
 
-  has_many :seller_transactions, foreign_key: :buyer_id, class_name: "Transaction"
+  has_many :listings
+
+  has_many :seller_transactions, through: :buyers, foreign_key: "buyer_id", class_name: "Listing"
+  has_many :buyer_transactions, through: :sellers, foreign_key: "seller_id", class_name: "Listing"
   has_many :buyers, through: :seller_transactions
-  has_many :items_on_sale, through: :seller_transactions
-  
-  has_many :buyer_transactions, foreign_key: :seller_id, class_name: "Transaction"
   has_many :sellers, through: :buyer_transactions
-  
-  has_many :transactions, dependent: :destroy
 
   validates :username, :location, presence: true
   validates :username, uniqueness: true
