9f549324b694178517c7a28893a0fa1c2d3a66d6
Adeja <alendor6@gmail.com>
2020-05-11 01:44:38 -0400

    add resources for listings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  
   namespace :api do
     namespace :v1 do
       resources :transactions
+      resources :listings
       resources :users
       resources :auth
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
