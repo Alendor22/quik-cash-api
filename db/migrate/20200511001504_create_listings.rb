23f7b0ee4ff2b804bf5287e5b5c5f32e3f72ae50
Adeja <alendor6@gmail.com>
2020-05-11 01:45:49 -0400

    add table

---
+++ db/migrate/20200511001504_create_listings.rb
@@ -0,0 +1,13 @@
+class CreateListings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :listings do |t|
+      t.string :item_name
+      t.text :description
+      t.float :price
+      t.integer :seller_id, foreign_key: true
+      t.integer :buyer_id, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
