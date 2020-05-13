7ae8b7e7ca76599f06b8533d95250de335f21c2d
Adeja <alendor6@gmail.com>
2020-05-13 18:37:30 -0400

    nested listings under users, added route to login

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :transactions
+      post 'login', to: "auth#login"
+      resources :users do
       resources :listings
-      resources :users
-      resources :auth
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+      end
     end
   end
+  
 end
