d881cdd9f0f1bfe01bb89f6a0715b4be6b909219
Adeja <alendor6@gmail.com>
2020-05-13 18:32:47 -0400

    remove @ from listing\s, add ternary to create

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -1,47 +1,49 @@
 class Api::V1::ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :update, :destroy]
+  before_action :render_unauthorized_response, only: [:create, :update, :destroy]
 
   # GET /listings
   def index
-    @listings = Listing.all
+    listings = Listing.all
 
-    render json: @listings
+    render json: listings, include: [:buyer, :seller], except: [:created_at, :updated_at]
   end
 
   # GET /listings/1
   def show
-    render json: @listing
+    render json: listing, include: [:buyer, :seller], except: [:created_at, :updated_at]
   end
 
   # POST /listings
   def create
-    @listing = Listing.new(listing_params)
-
-    if @listing.save
-      render json: @listing, status: :created, location: @listing
+    current_user ?
+    listing = Listing.new(listing_params)
+    if listing.save
+      render json: listing, status: :created, location: listing
+    : render_unauthorized_response
     else
-      render json: @listing.errors, status: :unprocessable_entity
+      render json: listing.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /listings/1
   def update
-    if @listing.update(listing_params)
-      render json: @listing
+    if listing.update(listing_params)
+      render json: listing
     else
-      render json: @listing.errors, status: :unprocessable_entity
+      render json: listing.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /listings/1
   def destroy
-    @listing.destroy
+    listing.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_listing
-      @listing = Listing.find(params[:id])
+      listing = Listing.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
