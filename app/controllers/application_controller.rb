965052fe34cbc8d981f135fabf53818519c0bb77
Adeja <alendor6@gmail.com>
2020-05-13 18:29:47 -0400

    add unauthorized\current_user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
 class ApplicationController < ActionController::API
 
   def secret
-    ENV['secret']
+    ENV["TOKEN_SECRET"]
+  end
+
+  def render_unauthorized_response
+    if !current_user
+    render json: {error: "You must be logged in for that action."},
+    status: 401
+    end
+  end
+
+  def current_user
+    if request.headers['Authorization']
+      encoded_token = request.headers['Authorization'].split(' ')[1]
+      token = JWT.decode(encoded_token, secret)
+      user_id = token[0]['user_id']
+      @user = User.find_by_id(user_id)
+    end
+    return @user
   end
 
 end
