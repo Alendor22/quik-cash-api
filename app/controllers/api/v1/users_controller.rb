4eab09efa73ad810ef4dc607bccb999494e71711
Adeja <alendor6@gmail.com>
2020-05-13 18:34:39 -0400

    add serialization to create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,57 +3,54 @@ class Api::V1::UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
-
-    render json: @users, include: [:transactions]
+    users = User.all
+    render json: users, include: [:seller_transactions, :buyer_transactions], except: [:password_digest, :created_at, :updated_at]
   end
 
   # GET /users/1
   def show
-    render json: @user, include: [:transactions]
+    render json: user, include: [:seller_transactions, :buyer_transactions], except: [:password_digest, :created_at, :updated_at]
   end
 
   # POST /users
   def create
-    @user = User.create(user_params)
-
-    if @user.valid?
-        @user = @user
+    user = User.create(user_params)
+    if user.valid?
         token = JWT.encode({user_id: user.id}, secret, 'HS256')
-      render json: {user: user, token: token}
-      status: :created
+        render json: {user: user.as_json(except: [:password_digest, :created_at, :updated_at]), token: token}
     else
-      render json: @user.errors.full_messages, status: :unprocessable_entity
+      render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /users/1
   def update
-    if @user.update(user_params)
-      render json: @user
+    if  user.update(user_params)
+      render json:  user
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json:  user.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /users/1
   def destroy
-    if  @user.destroy
+    if user.destroy
       render json: {message: "User Deleted"},
       status: 200
     else
-      render json {message: "User Failed To Delete"},
+      render json: {message: "User Failed To Delete"},
       status: :unprocessable_entity
+    end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+        user = User.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :password, :location, :bio, :email)
+      params.permit(:username, :password)
     end
 end
