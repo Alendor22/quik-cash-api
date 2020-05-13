6c137c63b33e65ecb9b9a71ae216377dfa228d59
Adeja <alendor6@gmail.com>
2020-05-13 18:36:08 -0400

    add source: to relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,20 @@
 class User < ApplicationRecord
 
-  has_many :listings
+  # has_many :listings
+  
+  has_many :seller_transactions, class_name: "Listing", foreign_key: "seller_id"
+  has_many :buyers, through: :seller_transactions, source: :buyer
 
-  has_many :seller_transactions, through: :buyers, foreign_key: "buyer_id", class_name: "Listing"
-  has_many :buyer_transactions, through: :sellers, foreign_key: "seller_id", class_name: "Listing"
-  has_many :buyers, through: :seller_transactions
-  has_many :sellers, through: :buyer_transactions
-
-  validates :username, :location, presence: true
+  has_many :buyer_transactions, class_name: "Listing", foreign_key: "buyer_id"
+  has_many :sellers, through: :buyer_transactions, source: :seller
+  
+  validates :username, presence: true
   validates :username, uniqueness: true
 
   has_secure_password
+
+  # def all_listings
+  #   buyer_transactions + seller_transactions
+  # end
 
 end
