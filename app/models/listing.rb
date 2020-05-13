e08efd98df6307e4f3dc693f4200c92f51435659
Adeja <alendor6@gmail.com>
2020-05-13 18:35:23 -0400

    add optional: true on the buyer relationship

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,7 @@
 class Listing < ApplicationRecord
 
-  belongs_to :seller, class_name: "User"
-  belongs_to :buyer, class_name: "User"
+  belongs_to :seller, class_name: "User", foreign_key: "seller_id"
+  belongs_to :buyer, class_name: "User", foreign_key: "buyer_id", optional: true
   
   validates :item_name, :description, :price, presence: true
   validates :price, numericality: true
