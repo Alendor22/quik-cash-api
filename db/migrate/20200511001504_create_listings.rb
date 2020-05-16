06f262b4fb6a4725c7310ba242dc96c7a83bed91
Adeja <alendor6@gmail.com>
2020-05-16 14:15:14 -0400

    add boolean :sold, default: false to table

--- db/migrate/20200511001504_create_listings.rb
+++ db/migrate/20200511001504_create_listings.rb
@@ -4,6 +4,7 @@ class CreateListings < ActiveRecord::Migration[6.0]
       t.string :item_name
       t.text :description
       t.float :price
+      t.boolean :sold, default: false
       t.integer :seller_id, foreign_key: true
       t.integer :buyer_id, foreign_key: true
 
