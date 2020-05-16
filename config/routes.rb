8f1c8cd892e675b607400aab75f8d49e7ea733a3
Adeja <alendor6@gmail.com>
2020-05-16 14:12:59 -0400

    add resorces :listings

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'login', to: "auth#login"
+      resources :listings
       resources :users do
       resources :listings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
