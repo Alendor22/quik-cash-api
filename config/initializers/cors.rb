8c7b8b822ead0a68f037b71e187ab48f0f65cab6
Alendor22 <alendor6@gmail.com>
2020-09-10 22:36:20 -0400

    working heroku backend_3

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://quik-cash-api.herokuapp.com/'
 
     resource '*',
       headers: :any,
