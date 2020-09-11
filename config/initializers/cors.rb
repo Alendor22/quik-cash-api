8fdc75d0c78ea07e7d0f936fce785b7daeb3e43f
Alendor22 <alendor6@gmail.com>
2020-09-11 10:43:07 -0400

    add github url to origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://quik-cash-api.herokuapp.com/'
+    origins 'https://alendor22.github.io/'
 
     resource '*',
       headers: :any,
