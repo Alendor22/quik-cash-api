9657b16c0e6accb8241cf221bc0abe0254e3ea5c
Alendor22 <alendor6@gmail.com>
2020-09-12 12:24:57 -0400

    add heroku as origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://alendor22.github.io/'
+    origins 'https://quik-cash.herokuapp.com/'
 
     resource '*',
       headers: :any,
