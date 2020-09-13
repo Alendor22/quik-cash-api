3a98c5344e02232a41a945251dd376b0aec7bf2b
Alendor22 <alendor6@gmail.com>
2020-09-13 17:46:07 -0400

    delete heroku url w\api/v1/listings cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://quik-cash.herokuapp.com/api/v1/listings'
+    origins 'https://quik-cash.herokuapp.com/'
 
     resource '*',
       headers: :any,
